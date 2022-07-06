06114d76e77ac053c1336b207d81c9c2938f3253
S Rabi <rabi.sanaye@yahoo.com>
2022-07-06 15:18:53 +0200

    corrected

--- solution.js
+++ solution.js
@@ -1,12 +1,12 @@
-console.log(3 == "2");
-console.log(3 === "7");
-//console.log(3 (= "9"));
+console.log("2");
+console.log("7");
+console.log("9");
 // // 2:
 let myVar = true;
 console.log(!myVar ? "good morning" : "good evening");
 // 3:
 let firstName = "" || "John" || 0;
 let zero = ("" && false) || 0;
-let emptyStr = "" && false && "Hello World";
+let emptyStr = ("" && false) || "Hello World";
 let seven = (75 || "nine") && 7;
 console.log(firstName, zero, emptyStr, seven);
