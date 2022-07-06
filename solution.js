664814dac8c456f2813778d3b76dfcae25a02503
S Rabi <rabi.sanaye@yahoo.com>
2022-07-06 15:42:17 +0200

    c

--- solution.js
+++ solution.js
@@ -1,12 +1,14 @@
 console.log(3 == "3");
-console.log(3 === "7");
-console.log(3 = ("9"));
+
+console.log(`3` == `"3"`);
+console.log(`3` === `"3"`);
+const value = 3;
 // // 2:
 let myVar = true;
 console.log(!myVar ? "good morning" : "good evening");
 // 3:
 let firstName = "" || "John" || 0;
 let zero = ("" && false) || 0;
-let emptyStr = ("" && false) || "Hello World";
+let emptyStr = "" && false && "Hello World";
 let seven = (75 || "nine") && 7;
 console.log(firstName, zero, emptyStr, seven);
